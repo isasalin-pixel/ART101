41cdafbb115093ec5619a7901e10b108b4269e0a
isasalin-pixel <isasalin@ucsc.edu>
2025-11-18 16:05:02 -0800

    group

--- exercises/garden/creature.js
+++ exercises/garden/creature.js
@@ -1,25 +1,41 @@
-// a variable for strong names
 
-//when the button is clicked (event)
 
 $("#add-creature").click(
-    function(){
+    function () {
 
-        //grab the value from input
-    let crName = $("#crName").val();
+        // grab the value from the input
+        let crName = $("#crName").val();
+        let crColor= $("#crColor").val();
+        let crEyesNum= $("#crEyesNum").val();
+        let crEyesHTML="";
+        let crTeethNum= $("#crTeethNum").val();
+        let crTeethHTML="";
 
-    //check the field value do not add empty ones
-    if (crName=="") || (crName.length>12)
-        {//do nothing }
-    else {
-         $("#creature-list").append(crName + ", ");
-    }     
+        for (let i = 0; i < crEyesNum ; i++){
+            crEyesHTML=crEyesHTML + "<div class=eye>.</div>";
+        }
+
+         // generate teeth
+         for (let i = 0; i < crTeethNum ; i++){
+        crTeethHTML += "<div class='teeth'>_</div>";
+        }
 
 
-    //remove the name after it's added
-    $("#crName").val("");
+        // check for the field value do not add empty ones
+        if ( (crName == "") || (crName.length>12) ) { // do nothing 
+        }
+        else {
+            $("#creature-list").append(`
+<div class="creature">
+    <div class="creature-body" style="background: ${crColor}">${crEyesHTML} 
+    ${crTeethHTML}</div>
+    <div class="creature-info">${crName}</div>
+</div>
+`);
+        }
 
-}});
+        // remove the name after it's added
+        $("#crName").val("");
 
-//the magic move
-//
+    });
+
